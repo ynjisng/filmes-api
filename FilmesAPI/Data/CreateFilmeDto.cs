8be17ef9035ba6f35f70c23e4a5e483124d43191
ynjisng <abreusara012@gmail.com>
2022-05-21 00:45:52 -0400

    feat: add-migration e configuração dos dados a serem buscado no dba

---
+++ FilmesAPI/Data/CreateFilmeDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class CreateFilmeDto
+    {
+        [Required(ErrorMessage = "O campo 'Título' não pode ser vazio")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "O campo 'Diretor' não pode ser vazio")]
+        public string Diretor { get; set; }
+
+        [StringLength(100, ErrorMessage = "O campo 'Gênero' não pode passar de 100 caracteres")]
+        public string Genero { get; set; }
+
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 e no máximo 600 minutos")]
+        public int Duracao { get; set; }
+    }
+}
--- FilmesAPI/Program.cs
+++ FilmesAPI/Program.cs
@@ -1,8 +1,17 @@
+using FilmesAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+string connString = builder.Configuration.GetConnectionString("FilmeConnection");
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
 
 // Add services to the container.
 
+builder.Services.AddDbContext<FilmeContext>(opts => opts.UseMySql(connString, serverVersion));
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
